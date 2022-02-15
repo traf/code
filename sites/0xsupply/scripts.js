6cbbc5904ad80edd19b9f966f263c87d72974d99
traf <trafj@me.com>
2022-02-15 13:40:39 -0500

    0xstack site update

---
+++ sites/0xsupply/scripts.js
@@ -0,0 +1,31 @@
+// Active filter class
+function onPageLoad() {
+    const header = document.querySelector('.notion-header');
+    const options = document.querySelectorAll('.notion-dropdown__option')
+    const currentTab = document.querySelector('.notion-dropdown__button-title').textContent
+    const setActiveTab = () => {
+        const active = Array.from(options).find(el => el.textContent === currentTab);
+        active.classList.add('active-filter')
+        options.forEach((option) => {
+            option.addEventListener('click', () => {
+                const activeFilter = document.querySelector('.active-filter')
+                if(activeFilter) { 
+                    activeFilter.classList.remove('active-filter')
+                }
+                option.classList.add('active-filter')
+            })
+        })
+    }
+    setActiveTab()
+        const config = { subtree: true, characterData: true };
+        const callback = function(mutationsList, observer) {
+        for (const mutation of mutationsList) {
+            if (mutation.type === 'characterData') {
+                setActiveTab()
+            }
+        }
+    };
+    const observer = new MutationObserver(callback);
+    observer.observe(header, config);
+}
+document.addEventListener("DOMContentLoaded", onPageLoad);
