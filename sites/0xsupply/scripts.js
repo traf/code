820f763d012cb675fdc0f74ed26e875e93442324
traf <trafj@me.com>
2022-02-16 18:09:31 -0500

    0xstack site update

--- sites/0xsupply/scripts.js
+++ sites/0xsupply/scripts.js
@@ -1,13 +1,3 @@
-// Smooth scroll on filter click
-const options = document.querySelectorAll('.notion-dropdown__option')
-if(document.documentElement.scrollTop > 400) {
-  options.forEach((option) => {
-    option.addEventListener('click', () => {
-       window.scrollTo({top: 400, behavior: "smooth"})
-    })  
-  }) 
-} 
-
 // Active filter class
 function onPageLoad() {
     const header = document.querySelector('.notion-header');
@@ -23,6 +13,10 @@ function onPageLoad() {
                     activeFilter.classList.remove('active-filter')
                 }
                 option.classList.add('active-filter')
+
+                if(document.documentElement.scrollTop > 400) {
+                    window.scrollTo({top: 400, behavior: "smooth"})
+                }
             })
         })
     }
@@ -39,3 +33,46 @@ function onPageLoad() {
     observer.observe(header, config);
 }
 document.addEventListener("DOMContentLoaded", onPageLoad);
+
+
+// // Smooth scroll on filter click
+// const options = document.querySelectorAll('.notion-dropdown__option')
+// if(document.documentElement.scrollTop > 400) {
+//   options.forEach((option) => {
+//     option.addEventListener('click', () => {
+//        window.scrollTo({top: 400, behavior: "smooth"})
+//     })  
+//   }) 
+// } 
+
+// // Active filter class
+// function onPageLoad() {
+//     const header = document.querySelector('.notion-header');
+//     const options = document.querySelectorAll('.notion-dropdown__option')
+//     const currentTab = document.querySelector('.notion-dropdown__button-title').textContent
+//     const setActiveTab = () => {
+//         const active = Array.from(options).find(el => el.textContent === currentTab);
+//         active.classList.add('active-filter')
+//         options.forEach((option) => {
+//             option.addEventListener('click', () => {
+//                 const activeFilter = document.querySelector('.active-filter')
+//                 if(activeFilter) { 
+//                     activeFilter.classList.remove('active-filter')
+//                 }
+//                 option.classList.add('active-filter')
+//             })
+//         })
+//     }
+//     setActiveTab()
+//         const config = { subtree: true, characterData: true };
+//         const callback = function(mutationsList, observer) {
+//         for (const mutation of mutationsList) {
+//             if (mutation.type === 'characterData') {
+//                 setActiveTab()
+//             }
+//         }
+//     };
+//     const observer = new MutationObserver(callback);
+//     observer.observe(header, config);
+// }
+// document.addEventListener("DOMContentLoaded", onPageLoad);
